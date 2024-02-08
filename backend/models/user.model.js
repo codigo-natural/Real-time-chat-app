768054fd833af54a7b088f1abf0060bae5423ad8
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-02-07 22:09:55 -0500

    add signup endpoint

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,26 +2,26 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   fullName: {
-    type: string,
+    type: String,
     required: true,
   },
   username: {
-    type: string,
+    type: String,
     required: true,
     unique: true,
   },
   password: {
-    type: string,
+    type: String,
     required: true,
     minlength: 6,
   },
   gender: {
-    type: string,
+    type: String,
     required: true,
     enum: ["male", "female"],
   },
   profilePic: {
-    type: string,
+    type: String,
     default: "",
   },
 });
