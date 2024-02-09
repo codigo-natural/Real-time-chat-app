61c5d5ba6578fbc4ec1fe35f2ca02bb44cbbf7e3
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-02-09 13:01:03 -0500

    create conversation model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -24,7 +24,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
-});
+  // createAt, updateAt -> Member since <createdAt>
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:8000/
