942f87e359092cd2eff18113cb47b841485f3500
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-02-10 20:46:37 -0500

    feat 🎉 IU Design for Home page chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { SignUp } from "./pages/signup/SignUp";
 
@@ -6,7 +7,8 @@ export default function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       {/* <Login /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Home />
     </div>
   )
 }
