ecf139583c4f006e12a78e7bc8034b9f4864737d
Ivan Camilo Buitrago <buitragoivancamilo1025@gmail.com>
2024-02-13 16:32:58 -0500

    feat 🎉: Get conversations

---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+export const useGetConversations = () => {
+  const [loading, setLoading] = useState(false);
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("/api/users");
+        const data = await response.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setConversations(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getConversations();
+  }, []);
+
+  return { loading, conversations };
+};
