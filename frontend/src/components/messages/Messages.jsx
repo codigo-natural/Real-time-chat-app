10b360c5d5c32f4c2dee24708502de30bf4f3372
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-02-15 17:49:42 -0500

    feat 🎉: implement socket.io and notification sound for new message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useRef } from "react"
 import { useGetMessages } from "../../hooks/useGetMessages"
 import { MessageSkeleton } from "../skeletons/MessageSkeleton"
+import { useListenMessages } from "../../hooks/useListenMessages"
 import { Message } from "./Message"
 
 export const Messages = () => {
   const { messages, loading } = useGetMessages()
+  useListenMessages()
   const lastMessageRef = useRef()
 
   useEffect(() => {
@@ -12,6 +14,7 @@ export const Messages = () => {
       lastMessageRef.current?.scrollIntoView({ behavior: "smooth" })
     }, 100);
   }, [messages])
+
   return (
     <div className="px-4 flex-1 overflow-auto">
       {!loading &&
@@ -21,9 +24,11 @@ export const Messages = () => {
             <Message message={message} />
           </div>
         ))}
+
       {loading && [...Array(3)].map((_, idx) => {
         <MessageSkeleton key={idx} />
       })}
+
       {!loading && messages.length === 0 && (
         <p className="text-center">Send a message to start the conversation</p>
       )}
